7b59ac305e0b6f20afddcd1bfc36fdd5309ac79a
Josiah Aubin <josiahaubin@gmail.com>
2019-08-20 14:54:27 -0600

    connected cat controller to cat service

--- controllers/CatController.js
+++ controllers/CatController.js
@@ -1,7 +1,11 @@
+import CatService from "../services/CatService.js";
 
+//PRIVATE
+let _catService = new CatService()
 
+//PUBLIC
 export default class CatController {
   constructor() {
-    console.log("Hello from cat controller")
+    //console.log("Hello from cat controller")
   }
 }
---
+++ services/CatService.js
@@ -0,0 +1,7 @@
+
+
+export default class CatService {
+  constructor() {
+    //console.log("Hello from cat service")
+  }
+}
